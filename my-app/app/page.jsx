3c0f3de655720305fffcde9fe7076e769494703e
CherniIyes <cherniiyes@gmail.com>
2025-02-07 21:13:58 +0100

    polishing

---
+++ my-app/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import HomePage from "../app/pages/Home/page"
+export default function Home() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
---
+++ my-app/app/pages/Home/page.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import Hero from "@/components/hero/Hero"
+// import AboutCard from "@/components/about/AboutCard"
+// import HAbout from "@/components/homeComponents/HAbout"
+// import Testimonal from ".@/components/testimonal/Testimonal"
+// import Hblog from "@/components/homeComponents/Hblog"
+// import Hprice from "@/components/homeComponents/Hprice"
+// import Faq from "@/components/pricing/Faq"
+
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      {/* <AboutCard /> */}
+      {/* <HAbout /> */}
+      {/* <Testimonal /> */}
+      {/* <Hblog /> */}
+      {/* <Hprice /> */}
+      {/* <Faq /> */}
+
+    </>
+
+  )
+}
+
+export default Home
